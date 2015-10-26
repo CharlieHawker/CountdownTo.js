d2a9786a98bd741b1a2604c64319b4221c686137
Charlie Hawker <charlie.hawker@staffordlong.co.uk>
2015-10-26 12:10:22 +0000

    initial commit

---
+++ countdownTo.js
@@ -0,0 +1,77 @@
+(function() {
+
+  var defaults = {
+    onCountdownBegin: function() {},
+    onCountdownEnd: function() {},
+    onCalculateRemainingTime: function() {},
+    updateInterval: 500,
+    indexTimeUnitNameMap: ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
+  };
+
+  Object.prototype.merge = function(object) {
+    var self = this;
+    for (var property in self) {
+      if ( self.hasOwnProperty(property) && object[property] === undefined )
+        object[property] = self[property];
+    }
+    return object;
+  };
+
+  var CountdownTo = function(endTime, options) {
+    this.options = defaults.merge(options || {});
+    this.endTime = (new Date(endTime)).getTime();
+    this.init();
+  };
+
+  CountdownTo.prototype.init = function() {
+    var c = this;
+
+    c.timeRemaining = Array(7).fill(0);
+    c.calculateTimeRemaining();
+    c.options.onCountdownBegin.call(c);
+    c.refreshInterval = setInterval(function() {
+      c.calculateTimeRemaining();
+    }, c.options.updateInterval);
+  };
+
+
+  CountdownTo.prototype.calculateTimeRemaining = function() {
+    var c = this,
+        r = c.endTime - (new Date()).getTime();
+
+    if ( r > 0 ) {
+      c.timeRemaining = [
+        Math.floor(r / (1000*60*60*24*365)), // years
+        Math.floor((r%(1000*60*60*24*365)) / (1000*60*60*24*30)), // months
+        Math.floor((r%(1000*60*60*24*30)) / (1000*60*60*24)), // days
+        Math.floor((r%(1000*60*60*24)) / (1000*60*60)), // hours
+        Math.floor((r%(1000*60*60)) / (1000*60)), // minutes
+        Math.floor((r%(1000*60)) / 1000); // seconds
+      ];
+
+      c.options.onCalculateRemainingTime.call(c);
+    } else {
+      clearInterval(c.refreshInterval);
+      c.timeRemaining = Array(7).fill(0);
+      c.options.onCountdownEnd.call(c);
+    }
+  };
+
+
+  CountdownTo.prototype.timeRemainingForDisplay = function() {
+    var c = this,
+        hasDisplayedSomething = false,
+        strs = [];
+
+    for (i in c.timeRemaining) {
+      if ( c.timeRemaining[i] > 0 || hasDisplayedSomething ) {
+        strs.push(c.timeRemaining[i] + ' ' + c.options.indexTimeUnitNameMap[i]);
+        hasDisplayedSomething = true;
+      }
+    }
+
+    return strs.join(' ');
+  };
+
+  window.CountdownTo = CountdownTo;
+})();
